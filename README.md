d568acab7f7d07718cde7981e6e599f1b62c71fe
Priya Jain <priyajain@yahoo.com>
2023-08-07 21:08:41 -0400

    Renamed app.py

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Group-Project-3
+# Group-Project-4
 Group-Project-4 for final challenge of module 23 of Datavis bootcamp
 
 ## Project Team
@@ -20,21 +20,16 @@ The project structure consists of the following files:
 3. `assets/styles.css` - A file to style the webpage. 
 4. `requirements.txt` - A file containing all the dependencies required to run the application.
 
-We've provided three interactive visualizations in our dashboard to show the following data:  
-  
-**Visualizations**
-- Quit, Hire and Job Opening Rates From 2001 - 2023: Line Chart 
-- Annual Quit Rates for Top 5 Industries During Covid-19: Bar Chart
-- Total Quit Rates by Industry During and After Recession: Bar Chart 
-  
-This interative dashboard explores the ["Bureau of Labor Statictics" dataset](https://www.bls.gov/). This dashboard contains charts to visualize BLS data that will allows us to better understand labor trends caused by major events such as the Covid-19 pendemic and the 2008 recession. 
 
 ## Built with
 - Dash API, with HTML and CSS 
+- Flask
+- Python
 - Pandas
 - Numpy
 - Plotly
 - yfinance
+- nltk 
 - Scikit-learn
 - Tableau
 - Azure Web Services
@@ -56,7 +51,7 @@ Make sure you have installed all of the following prerequisites on your developm
 - Clone this repo and save it in your local directory
 - git clone `https://github.com/priyajainnyc/group-project-4-azure`
 - Open the repo in a code editor to see the codes
-- Visit [localhost: 5000](http://localhost:5000) in your browser
+- Visit [localhost: 8050](http://127.0.0.1:8050/) in your browser
 
 ## Usage
 
@@ -64,10 +59,25 @@ To use the application, simply run the `importdash.py` file in your Python envir
 
 Once the user enters the stock code and date range, the application will fetch the relevant stock data using the yfinance library and plot the stock price data. Additionally, the user can select an option to view predicted stock prices based on a machine learning algorithm trained on the historical data.
 
-## Data Sources
-- Yahoo! Finance API 
-- [Bureau of Labor Statistics, Annual Average Quits Rates by Industry and Region, not Seasonally Adjuste](https://www.bls.gov/news.release/jolts.t22.htm)
+## AI Stock Analysis (72 Selected Stocks)
+
+We've provided the below listed visualizations for AI specific stock data pulled from Finviz in a Tableau dashboard as well as a sentiment analysis from the nltk library data:  
+  
+**Visualizations**
+- Market Cap With Team Recommendation Category 
+- Stock Current vs. Target Price 
+- Number Of Employees By Sector
+- Sales & Profit Margin % With Team Recommendation Category 
+- EPS Growth With Team Recommendation Category 
+- Performance % Past Year With Team Recommendation Category 
+- Target vs. Current Price With Team Recommendation Category 
+- Stock Price 52 Week Hi, Lo, Current vs. Target Price
 
 ## Conclusion
 
-This project provides a simple yet powerful tool for investors to visualize and analyze stock data of a specific company using machine learning models. It is a great project for beginners to learn about web application development, data visualization, and machine learning in Python.
+This project provides a simple yet powerful tool for investors to visualize and analyze stock data of a specific company using machine learning models. It is a great project for beginners to learn about web application development, data visualization, and machine learning in Python and Tableau.
+
+## Data Sources
+- Yahoo! Finance API 
+- NLTK Library
+- Finviz
---
+++ app.py
@@ -0,0 +1,184 @@
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Output, Input, State
+from dash.exceptions import PreventUpdate
+from datetime import datetime as dt
+import yfinance as yf
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.express as px
+
+# modelp
+from model import prediction
+
+from sklearn.svm import SVR
+
+app = dash.Dash(__name__, external_stylesheets=['assets\styles.css'])
+server = app.server
+
+# Define the layout components
+
+# Navigation component
+item1 = html.Div(
+    [
+        html.P("Welcome to the Stock Dash App!", className="start"),
+
+        html.Div([
+            # stock code input
+            dcc.Input(id='stock-code', type='text', placeholder='Enter stock code'),
+            html.Button('Submit', id='submit-button')
+        ], className="stock-input"),
+
+        html.Div([
+            # Date range picker input
+            dcc.DatePickerRange(
+                id='date-range', start_date=dt(2020, 1, 1).date(), end_date=dt.now().date(), className='date-input')
+        ]),
+        html.Div([
+            # Stock price button
+            html.Button('Get Stock Price', id='stock-price-button'),
+
+            # Indicators button
+            html.Button('Get Indicators', id='indicators-button'),
+
+            # Number of days of forecast input
+            dcc.Input(id='forecast-days', type='number', placeholder='Enter number of days'),
+
+            # Forecast button
+            html.Button('Get Prediction', id='forecast-button')
+        ], className="selectors")
+    ],
+    className="nav"
+)
+
+# Content component
+item2 = html.Div(
+    [
+        html.Div(
+            [
+                html.Img(id='logo', className='logo'),
+                html.H1(id='company-name', className='company-name')
+            ],
+            className="header"),
+        html.Div(id="description"),
+        html.Div([], id="graphs-content"),
+        html.Div([], id="main-content"),
+        html.Div([], id="forecast-content")
+    ],
+    className="content"
+)
+
+# Set the layout
+app.layout = html.Div(className='container', children=[item1, item2])
+
+# Callbacks
+
+# Callback to update the data based on the submitted stock code
+@app.callback(
+    [
+        Output("description", "children"),
+        Output("logo", "src"),
+        Output("company-name", "children"),
+        Output("stock-price-button", "n_clicks"),
+        Output("indicators-button", "n_clicks"),
+        Output("forecast-button", "n_clicks")
+    ],
+    [Input("submit-button", "n_clicks")],
+    [State("stock-code", "value")]
+)
+def update_data(n, val):
+    if n is None:
+        return None, None, None, None, None, None
+    else:
+        if val is None:
+            raise PreventUpdate
+        else:
+            ticker = yf.Ticker(val)
+            inf = ticker.info
+            if 'logo_url' not in inf:
+                return None, None, None, None, None, None
+            else:
+                name = inf['longName']
+                logo_url = inf['logo_url']
+                description = inf['longBusinessSummary']
+                return description, logo_url, name, None, None, None
+
+
+# Callback for displaying stock price graphs
+@app.callback(
+    [Output("graphs-content", "children")],
+    [
+        Input("stock-price-button", "n_clicks"),
+        Input('date-range', 'start_date'),
+        Input('date-range', 'end_date')
+    ],
+    [State("stock-code", "value")]
+)
+def stock_price(n, start_date, end_date, val):
+    if n is None:
+        return [""]
+    if val is None:
+        raise PreventUpdate
+    else:
+        if start_date is not None:
+            df = yf.download(val, str(start_date), str(end_date))
+        else:
+            df = yf.download(val)
+
+    df.reset_index(inplace=True)
+    fig = px.line(df, x="Date", y=["Close", "Open"], title="Closing and Opening Price vs Date")
+    return [dcc.Graph(figure=fig)]
+
+
+# Callback for displaying indicators
+@app.callback(
+    [Output("main-content", "children")],
+    [
+        Input("indicators-button", "n_clicks"),
+        Input('date-range', 'start_date'),
+        Input('date-range', 'end_date')
+    ],
+    [State("stock-code", "value")]
+)
+def indicators(n, start_date, end_date, val):
+    if n is None:
+        return [""]
+    if val is None:
+        return [""]
+    if start_date is None:
+        df_more = yf.download(val)
+    else:
+        df_more = yf.download(val, str(start_date), str(end_date))
+
+    df_more.reset_index(inplace=True)
+    fig = get_more(df_more)
+    return [dcc.Graph(figure=fig)]
+
+
+def get_more(df):
+    df['EWA_20'] = df['Close'].ewm(span=20, adjust=False).mean()
+    fig = px.scatter(df, x="Date", y="EWA_20", title="Exponential Moving Average vs Date")
+    fig.update_traces(mode='lines+markers')
+    return fig
+
+
+# Callback for displaying forecast
+@app.callback(
+    [Output("forecast-content", "children")],
+    [Input("forecast-button", "n_clicks")],
+    [State("forecast-days", "value"),
+     State("stock-code", "value")]
+)
+def forecast(n, n_days, val):
+    #print(f"n_days: {n_days}\n n: {n}\n val: {val}")
+    if n is None:
+        return [""]
+    if val is None:
+        raise PreventUpdate
+    fig = prediction(val, int(n_days) + 1)
+    return [dcc.Graph(figure=fig)]
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
